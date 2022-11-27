cad4034bf420377036367afba2af44d32c0d8583
ZoQsito <florianroman59@gmail.com>
2022-11-27 19:39:28 +0100

    first commit

---
+++ app/data/QuizData.js
@@ -0,0 +1,37 @@
+export default data = [
+    {
+        question: "Quel est la longeur de mon entre jambes",
+        options: ["12cm","6cm","34cm","2cm"],
+        correct_option: "2cm"
+    },
+    {
+        question: "Qu'est que une femme arabe avec des cacahuette dans le cul",
+        options: ["La Femme a rachid","La femme a Bouder","Patricia"],
+        correct_option: "Taj Mahal"
+    },
+    {
+        question: "What land animal can open its mouth the widest?",
+        options: ["Alligator","Crocodile","Baboon","Hippo"],
+        correct_option: "Hippo"
+    },
+    {
+        question: "What is the largest animal on Earth?",
+        options: ["The African elephant","The blue whale","The sperm whale","The giant squid"],
+        correct_option: "The blue whale"
+    },
+    {
+        question: "What is the only flying mammal?",
+        options: ["The bat","The flying squirrel","The bald eagle","The colugo"],
+        correct_option: "The bat"
+    },
+    {
+        question: "What is the only flying mammal?",
+        options: ["The bat","The flying squirrel","The bald eagle","The colugo"],
+        correct_option: "The bat"
+    },
+    {
+        question: "What is the only flying mammal?",
+        options: ["The bat","The flying squirrel","The bald eagle","The colugo"],
+        correct_option: "The bat"
+    }
+]
